1741f683ec1533a1ead220c9bf7354a15a6abd3a
julian <julian.stecher@student.uibk.ac.at>
2023-05-31 13:49:27 +0200

    added ttyUSB0

--- Frontend/ButtonExample.java
+++ Frontend/ButtonExample.java
@@ -1,32 +1,55 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import com.fazecast.jSerialComm.*;
 
 public class ButtonExample {
+    private static SerialPort serialPort;
+
     public static void main(String[] args) {
         // Create a new JFrame
         JFrame frame = new JFrame("Button Example");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+
         // Create a new JButton
-        JButton button = new JButton("Click me!");
-        
+        JButton button = new JButton("Send Command");
+
         // Set the layout manager of the JFrame to a FlowLayout
         frame.setLayout(new FlowLayout());
-        
+
         // Add the button to the JFrame
         frame.add(button);
-        
+
         // Create a new ActionListener for the button
         button.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 // Handle button click event here
-                JOptionPane.showMessageDialog(null, "Button clicked!");
+                sendCommandToSerialPort("ttyUSB0", "YOUR_COMMAND_HERE");
             }
         });
-        
+
         // Set the size of the JFrame and make it visible
         frame.setSize(300, 200);
         frame.setVisible(true);
+    }
+
+    public static void sendCommandToSerialPort(String portName, String command) {
+        serialPort = SerialPort.getCommPort(portName);
+        serialPort.setComPortParameters(9600, 8, 1, SerialPort.NO_PARITY);
+        serialPort.setComPortTimeouts(SerialPort.TIMEOUT_WRITE_BLOCKING, 0, 0);
+
+        if (serialPort.openPort()) {
+            try {
+                byte[] commandBytes = command.getBytes();
+                serialPort.getOutputStream().write(commandBytes);
+                System.out.println("Command sent: " + command);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            } finally {
+                serialPort.closePort();
+            }
+        } else {
+            System.out.println("Failed to open serial port.");
+        }
     }
 }
